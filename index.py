c8d7e17765fdbe3a1bede95ea577871791e54212
nithish <nithish98raja@gmail.com>
2021-06-05 19:39:42 +0530

    add logic to retry upon failure

--- index.py
+++ index.py
@@ -10,50 +10,59 @@ from src.day import getDay
 from src.quote import getQuote
 from src.song import getSong
 
-try:
-    # Call function to get day
-    day = getDay()
-    # Print day
-    print(day["text"])
-    # Open file
-    file = open("./data/day.json", "w")
-    # Write to file
-    json.dump(day, file)
-    # Close file
-    file.close()
-except:
-    print("failed to get day")
+while(True):
+    try:
+        # Call function to get day
+        day = getDay()
+        # Print day
+        print(day["text"])
+        # Open file
+        file = open("./data/day.json", "w")
+        # Write to file
+        json.dump(day, file)
+        # Close file
+        file.close()
+        # Exit loop
+        break
+    except:
+        print("Failed to get day. Trying again...")
 
-try:
-    # Call function to get quote
-    quote = getQuote()
-    # Print quote
-    print(quote["topic"])
-    print(quote["quote"])
-    print(quote["author"])
-    # Open file
-    file = open("./data/quote.json", "w")
-    # Write to file
-    json.dump(quote, file)
-    # Close file
-    file.close()
-except:
-    print("failed to get quote")
+while(True):
+    try:
+        # Call function to get quote
+        quote = getQuote()
+        # Print quote
+        print(quote["topic"])
+        print(quote["quote"])
+        print(quote["author"])
+        # Open file
+        file = open("./data/quote.json", "w")
+        # Write to file
+        json.dump(quote, file)
+        # Close file
+        file.close()
+        # Exit loop
+        break
+    except:
+        print("Failed to get quote. Trying again...")
 
-try:
-    # Call function to get song
-    song = getSong()
-    # Print song details
-    print(song["title"])
-    print(song["artist"])
-    for item in song["info"]:
-        print(item)
-    print(song["lyrics"])
-    # Open file
-    file = open("./data/song.json", "w")
-    # Write to file
-    json.dump(song, file)
-    # Close file
-    file.close()
-except:
-    print("failed to get song")
+while(True):
+    try:
+        # Call function to get song
+        song = getSong()
+        # Print song details
+        print(song["title"])
+        print(song["artist"])
+        for item in song["info"]:
+            print(item)
+        print(song["lyrics"])
+        # Open file
+        file = open("./data/song.json", "w")
+        # Write to file
+        json.dump(song, file)
+        # Close file
+        file.close()
+        # Exit loop
+        break
+    except:
+        print("Failed to get song. Trying again...")
