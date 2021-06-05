f9ab9b2fbe66649cff26679b835f1ef5db5eb653
nithish <nithish98raja@gmail.com>
2021-06-05 19:56:04 +0530

    implement threading to reduce latency

--- index.py
+++ index.py
@@ -4,65 +4,86 @@
 #
 
 # Dependencies
-import json
+import json, threading
 # Local dependencies
 from src.day import getDay
 from src.quote import getQuote
 from src.song import getSong
 
-while(True):
-    try:
-        # Call function to get day
-        day = getDay()
-        # Print day
-        print(day["text"])
-        # Open file
-        file = open("./data/day.json", "w")
-        # Write to file
-        json.dump(day, file)
-        # Close file
-        file.close()
-        # Exit loop
-        break
-    except:
-        print("Failed to get day. Trying again...")
+# Function to get song
+def day():
+    # Iterate till request is a success
+    while(True):
+        try:
+            # Call function to get day
+            day = getDay()
+            # Print day
+            print(day["text"])
+            # Open file
+            file = open("./data/day.json", "w")
+            # Write to file
+            json.dump(day, file)
+            # Close file
+            file.close()
+            # Exit loop
+            break
+        except:
+            # Print error message
+            print("Failed to get day. Trying again...")
 
-while(True):
-    try:
-        # Call function to get quote
-        quote = getQuote()
-        # Print quote
-        print(quote["topic"])
-        print(quote["quote"])
-        print(quote["author"])
-        # Open file
-        file = open("./data/quote.json", "w")
-        # Write to file
-        json.dump(quote, file)
-        # Close file
-        file.close()
-        # Exit loop
-        break
-    except:
-        print("Failed to get quote. Trying again...")
+# Function to get quote
+def quote():
+    # Iterate till request is a success
+    while(True):
+        try:
+            # Call function to get quote
+            quote = getQuote()
+            # Print quote
+            print(quote["topic"])
+            print(quote["quote"])
+            print(quote["author"])
+            # Open file
+            file = open("./data/quote.json", "w")
+            # Write to file
+            json.dump(quote, file)
+            # Close file
+            file.close()
+            # Exit loop
+            break
+        except:
+            # Print error message
+            print("Failed to get quote. Trying again...")
 
-while(True):
-    try:
-        # Call function to get song
-        song = getSong()
-        # Print song details
-        print(song["title"])
-        print(song["artist"])
-        for item in song["info"]:
-            print(item)
-        print(song["lyrics"])
-        # Open file
-        file = open("./data/song.json", "w")
-        # Write to file
-        json.dump(song, file)
-        # Close file
-        file.close()
-        # Exit loop
-        break
-    except:
-        print("Failed to get song. Trying again...")
+def song():
+    # Iterate till request is a success
+    while(True):
+        try:
+            # Call function to get song
+            song = getSong()
+            # Print song details
+            print(song["title"])
+            print(song["artist"])
+            for item in song["info"]:
+                print(item)
+            print(song["lyrics"])
+            # Open file
+            file = open("./data/song.json", "w")
+            # Write to file
+            json.dump(song, file)
+            # Close file
+            file.close()
+            # Exit loop
+            break
+        except:
+            # Print error message
+            print("Failed to get song. Trying again...")
+
+# Initialise threads
+dayThread = threading.Thread(target=day, args=[])
+quoteThread = threading.Thread(target=quote, args=[])
+songThread = threading.Thread(target=song, args=[])
+
+# Start threads
+dayThread.start()
+quoteThread.start()
+songThread.start()
