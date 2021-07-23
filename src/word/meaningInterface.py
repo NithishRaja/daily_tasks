785bc314a1b69e76f77f4396f6e8f71aa8050aeb
nithish <nithish98raja@gmail.com>
2021-07-23 15:33:02 +0530

    add comments specifying how interfaces should be implemented

--- src/word/meaningInterface.py
+++ src/word/meaningInterface.py
@@ -6,6 +6,12 @@
 # Dependencies
 from abc import ABC, abstractmethod
 
+# The class should always contain a getMeaning function
+# getMeaning function should accept a string as input
+# It should return a list of meanings of the word passed in the string
+# If the string does not contain a word or meaning doesn't exist, it should return an empty list
+# In case of any errors/failures, an empty list should be returned
+
 # Initialise meaning class
 class MeaningInterface(ABC):
 
--- src/word/wordInterface.py
+++ src/word/wordInterface.py
@@ -6,6 +6,12 @@
 # Dependencies
 from abc import ABC, abstractmethod
 
+# The class should always contain a getWord function
+# getWord function should return an object with the following fields
+# "word", "wordType", "pronunciation"
+# There should also be a default object with the same fields
+# The default object should be returned in case of any errors/failures
+
 # Initialise word class
 class WordInterface(ABC):
 
